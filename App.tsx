66aede5a2bd09c304af4425519b6bd5a54e6eb22
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-11-03 00:07:23 -0400

    update to the  app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
-
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+  View,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {GradebookContext} from './utils/GradebookContext';
+import HomeScreen from './screens/HomeScreen';
 import FirebaseFetcher from './FirebaseFetcher';
+import GradebookScreen from './screens/GradeBookScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [thresholds, setThresholds] = useState({
+    APlus: 97.5,
+    A: 90,
+    B: 80,
+    C: 70,
+  });
 
-  const backgroundStyle = {
+  const backgroundStyle: StyleProp<ViewStyle> = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
   return (
@@ -17,9 +33,13 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <Text>
-        <FirebaseFetcher />
-      </Text>
+      <GradebookContext.Provider value={{thresholds, setThresholds}}>
+        <View style={{flex: 1}}>
+          {/* <FirebaseFetcher /> */}
+          <HomeScreen />
+          <GradebookScreen />
+        </View>
+      </GradebookContext.Provider>
     </SafeAreaView>
   );
 }
